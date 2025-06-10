b6588da27c9d047ecb81853503cd39c119650e32
Dhana-QAAnalyst <dhanalakshmisugirtha@gmail.com>
2025-06-10 18:51:54 -0400

    added .env config

--- handson/src/main/java/Selenium_Test/TC_06_BrowserStack_ParallelExecution.java
+++ handson/src/main/java/Selenium_Test/TC_06_BrowserStack_ParallelExecution.java
@@ -3,9 +3,9 @@ package Selenium_Test;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -15,9 +15,11 @@ public class TC_06_BrowserStack_ParallelExecution {
 	@Test(description = "TestID: 01 - Verify parallel execution using BrowserStack - Firefox broswer")
 	public void Test01_Firefox() throws Exception {
 		
+		Dotenv dot = Dotenv.load();
+		
 		String baseurl = "https://ultimateqa.com/automation";
-		String username = "dhanalakshmikris_rXWcKq";
-		String accessKey = "XhD4nxYR24uD4ZsP9pGd";
+		String username = dot.get("BROWSERSTACK_USERNAME");
+		String accessKey = dot.get("BROWSERSTACK_ACCESS_KEY");
 		String browserStackURL = "https://"+username+":"+accessKey+"@hub-cloud.browserstack.com/wd/hub";
 		
 		FirefoxOptions options = new FirefoxOptions();
